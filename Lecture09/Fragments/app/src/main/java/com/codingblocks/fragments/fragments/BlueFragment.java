a7b39754aa701b1126021ab44efaf046d20a6107
Arnav Gupta <arnav@codingblocks.com>
2017-09-17 17:37:05 +0530

    basic fragments
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture09/Fragments/app/src/main/java/com/codingblocks/fragments/fragments/BlueFragment.java
@@ -0,0 +1,30 @@
+package com.codingblocks.fragments.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codingblocks.fragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlueFragment extends Fragment {
+
+
+    public BlueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blue, container, false);
+    }
+
+}
