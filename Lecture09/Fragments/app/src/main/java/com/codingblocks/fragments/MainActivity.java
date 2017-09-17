74daf37a0e9d287f4649c552c65384865fb470f8
Arnav Gupta <arnav@codingblocks.com>
2017-09-17 18:44:40 +0530

    click to change fragments
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture09/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivity.java
+++ Lecture09/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivity.java
@@ -1,13 +1,48 @@
 package com.codingblocks.fragments;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.codingblocks.fragments.fragments.BlueFragment;
+import com.codingblocks.fragments.fragments.RedFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    FragmentManager fragmentManager;
+    BlueFragment blueFragment;
+    RedFragment redFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_fragments);
+        setContentView(R.layout.activity_main_dynamic_fragments);
+
+        fragmentManager = getSupportFragmentManager();
+
+        blueFragment = new BlueFragment();
+        redFragment = new RedFragment();
+
+        findViewById(R.id.btnBlue).setOnClickListener(this);
+        findViewById(R.id.btnRed).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnBlue:
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.flFragContainer, blueFragment)
+                        .commit();
+                break;
+            case R.id.btnRed:
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.flFragContainer, redFragment)
+                        .commit();
+                break;
+        }
     }
 }
