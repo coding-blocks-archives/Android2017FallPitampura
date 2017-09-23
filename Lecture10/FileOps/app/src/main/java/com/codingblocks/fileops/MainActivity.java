40cf3eb31acaa02cf934e6751d5293343623de90
Arnav Gupta <arnav@codingblocks.com>
2017-09-23 19:05:02 +0530

    file paths logs
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture10/FileOps/app/src/main/java/com/codingblocks/fileops/MainActivity.java
+++ Lecture10/FileOps/app/src/main/java/com/codingblocks/fileops/MainActivity.java
@@ -1,7 +1,9 @@
 package com.codingblocks.fileops;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvFileData;
     EditText etNewData;
     Button btnSave, btnRetrieve;
-
+    public static final String TAG = "FILE";
     public static final String FILE_NAME = "myfile.txt";
 
     @Override
@@ -32,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
         etNewData = (EditText) findViewById(R.id.etNewData);
         btnRetrieve = (Button) findViewById(R.id.btnRetrieve);
         btnSave = (Button) findViewById(R.id.btnSave);
+
+        Log.d(TAG, "context ======= ");
+        Log.d(TAG, "getFilesDir: " + getFilesDir());
+        Log.d(TAG, "getExternalFilesDir(null): " + getExternalFilesDir(null));
+
+        Log.d(TAG, "Environment ======== ");
+        Log.d(TAG, "getDataDirectory: " + Environment.getDataDirectory());
+        Log.d(TAG, "getExternalStorageDirectory: "
+                + Environment.getExternalStorageDirectory());
+
+        for (File f : getExternalFilesDirs(null)) {
+            Log.d(TAG, "getExternalFilesDirs" + f);
+        }
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     void saveData (String data) throws IOException {
-        File f = new File (getFilesDir(), FILE_NAME);
+        File f = new File (Environment.getExternalStorageDirectory(), FILE_NAME);
         FileOutputStream fos = new FileOutputStream(f, true);
         fos.write(("\n" + data).getBytes());
         fos.close();
@@ -68,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
     String retrieveData () throws IOException {
 
-        File f = new File(getFilesDir(), FILE_NAME);
+        File f = new File(Environment.getExternalStorageDirectory(), FILE_NAME);
         FileInputStream fis = new FileInputStream(f);
 
         String buf = "";
