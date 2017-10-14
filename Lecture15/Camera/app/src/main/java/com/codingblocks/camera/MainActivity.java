4f837d54de41267ce6b99da887230744259b0451
Arnav Gupta <arnav@codingblocks.com>
2017-10-14 18:36:23 +0530

    camera rotation
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture15/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
+++ Lecture15/Camera/app/src/main/java/com/codingblocks/camera/MainActivity.java
@@ -3,13 +3,17 @@ package com.codingblocks.camera;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     Button btnPreview, btnCapture;
@@ -24,28 +28,14 @@ public class MainActivity extends AppCompatActivity {
         btnCapture = (Button) findViewById(R.id.btnCapture);
         btnPreview = (Button) findViewById(R.id.btnPreview);
         surfaceHolder = surfaceView.getHolder();
-
-        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+        btnPreview.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                try {
-                    camera.setPreviewDisplay(surfaceHolder);
-                    camera.startPreview();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+            public void onClick(View view) {
+                camera.startPreview();
+                camera.setDisplayOrientation(90);
             }
         });
+
 
     }
 
@@ -61,11 +51,59 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+        surfaceHolder.addCallback(this);
     }
 
     @Override
     protected void onPause() {
         camera.release();
         super.onPause();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            camera.stopPreview();
+        } catch ( Exception e) {}
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            switch (getWindowManager().getDefaultDisplay().getRotation()) {
+                case Surface.ROTATION_0:
+                    camera.setDisplayOrientation(90);
+                    break;
+                case Surface.ROTATION_90:
+                    camera.setDisplayOrientation(0);
+                    break;
+                case Surface.ROTATION_180:
+                    camera.setDisplayOrientation(270);
+                    break;
+                case Surface.ROTATION_270:
+                    camera.setDisplayOrientation(180);
+                    break;
+
+            }
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
     }
 }
