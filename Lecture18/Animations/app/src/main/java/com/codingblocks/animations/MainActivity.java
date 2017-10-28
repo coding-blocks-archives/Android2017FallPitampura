57c97920504505abc939f1027a6c6cc9dc9f4bf6
Arnav Gupta <arnav@codingblocks.com>
2017-10-28 17:11:27 +0530

    transition of shared elements
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture18/Animations/app/src/main/java/com/codingblocks/animations/MainActivity.java
+++ Lecture18/Animations/app/src/main/java/com/codingblocks/animations/MainActivity.java
@@ -4,14 +4,23 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgBackup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgBackup = findViewById(R.id.imgBackup);
+
+
 
         findViewById(R.id.btnSwitch).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,9 +29,22 @@ public class MainActivity extends AppCompatActivity {
                         MainActivity.this,
                         SecondActivity.class
                 );
-                startActivity(i,
-                        ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+                ActivityOptions transitionOptions =
+                        ActivityOptions.makeSceneTransitionAnimation(
+                                MainActivity.this, imgBackup, "backup");
+
+                startActivity(i, transitionOptions.toBundle());
+            }
+        });
+
+        findViewById(R.id.btnFinish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.slide_up, R.anim.slide_up);
+
             }
         });
     }
+
 }
--- Lecture18/Animations/app/src/main/java/com/codingblocks/animations/SecondActivity.java
+++ Lecture18/Animations/app/src/main/java/com/codingblocks/animations/SecondActivity.java
@@ -2,12 +2,16 @@ package com.codingblocks.animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
+    ImageView imgBackup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        imgBackup = findViewById(R.id.imgBackup);
     }
 }
